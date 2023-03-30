b2edf8b90b9c06d6d7f9b59cedeabd203f55a429
Viacheslav Kharchenko <slava.xarchenko@gmail.com>
2023-03-30 12:11:00 +0300

    Init commit Project settings

---
+++ src/main/java/ua/slava/flyhub/FlyHubApplication.java
@@ -0,0 +1,13 @@
+package ua.slava.flyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/slava/flyhub/FlyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.slava.flyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
